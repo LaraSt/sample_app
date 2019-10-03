76598663d2581863533c22eb566ce4ecc0d2c85b
LaraSt <larysa@cloverpop.com>
2019-10-04 02:36:17 +0300

    Finished Updating Users

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ ruby '2.4.2'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 gem 'bcrypt', '3.1.7'
+gem 'faker', '~> 2.5'
+gem 'will_paginate', '~> 3.1', '>= 3.1.8'
+gem 'bootstrap-will_paginate', '~> 1.0'
 gem 'bootstrap-sass', '3.2.0.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
@@ -13,7 +16,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
-gem 'rails-controller-testing'
+gem 'rails-controller-testing', '~> 1.0', '>= 1.0.4'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,43 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+  
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+  end
+  
   test "should get new" do
     get :new
     assert_response :success
   end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete :destroy, params: {id: @user.id}
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+    delete :destroy, params: {id: @user.id}
+    end
+    assert_redirected_to root_url
+  end
+
+    #test "should not allow the admin attribute to be edited via the web" do
+      #log_in_as(@other_user)
+      # {assert_not @other_user.admin?
+      # patch :update, id: @other_user, params: {user: { password: "foobar",
+      #                                         password_confirmation: "foobar",
+      #                                         admin: false }}
+      # assert_not @other_user.flash.admin?}
+    #end
 end
