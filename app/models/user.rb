9ee795d538fe1b5e215f9c666f689a2d38564998
LaraSt <larysa@cloverpop.com>
2019-10-27 00:49:19 +0300

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,43 +1,88 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token, :reset_token
+  #before_save { self.email = email.downcase }
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-format: { with: VALID_EMAIL_REGEX },
-uniqueness: { case_sensitive: false }
-has_secure_password
-validates :password, length: { minimum: 6 }, allow_blank: true
+  format: { with: VALID_EMAIL_REGEX },
+  uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6 }, allow_blank: true
 
 class << self
-# Возвращает хэш - дайджест для указанной строки.
-def digest(string)
-  cost = ActiveModel::SecurePassword.min_cost ?
-  BCrypt::Engine::MIN_COST :
-  BCrypt::Engine.cost
-  BCrypt::Password.create(string, cost: cost)
-end
+  # Возвращает хэш - дайджест для указанной строки.
+  def digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ?
+    BCrypt::Engine::MIN_COST :
+    BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 
-# Возвращает случайный токен.
-def new_token
-  SecureRandom.urlsafe_base64
-end
-end
+  # Возвращает случайный токен.
+  def new_token
+    SecureRandom.urlsafe_base64
+    end
+  end
 
-# Запоминает пользователя в базе данных для использования в постоянных сеансах.
-def remember
-  self.remember_token = User.new_token
-  update_attribute(:remember_digest, User.digest(remember_token))
-end
+  # Запоминает пользователя в базе данных для использования в постоянных сеансах.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
 
-# Возвращает true, если указанный токен соответствует дайджесту.
-def authenticated?(remember_token)
-  return false if remember_digest.nil?
-  BCrypt::Password.new(remember_digest).is_password?(remember_token)
-end
+  # Возвращает true, если указанный токен соответствует дайджесту.
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
 
-# Забывает пользователя
-def forget
-  update_attribute(:remember_digest, nil)
-end
+  # Забывает пользователя
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  # Активирует учетную запись.
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Посылает письмо со ссылкой на страницу активации.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  # Устанавливает атрибуты для сброса пароля.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Посылает письмо со ссылкой на форму ввода нового пароля.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # Возвращает true, если время для сброса пароля истекло.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+  private
+
+  # Преобразует адрес электронной почты в нижний регистр.
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # Создает и присваивает токен активации и его дайджест.
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
 end
